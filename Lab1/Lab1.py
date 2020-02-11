5e0c076f02a2083c76935b4c85d77f71dcb27028
DovzhenkoVit <dovzhenko.vit@gmail.com>
2020-02-11 23:49:48 +0200

    implement basic logic but still does not working

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,18 +1,38 @@
 from PyQt5 import QtWidgets
- 
+from math import sqrt
 # Импортируем наш шаблон.
 from Lab1_view import Ui_MainWindow
  
 import sys
- 
- 
+
+
+def _herons_formula(a, b, c):
+    p = (a + b + c) / 2
+    return sqrt(p * (p - a) * (p - b) * (p - c))
+
+
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
- 
- 
+
+        self.ui.pushButton.clicked.connect(self.btnClicked)
+
+    def btnClicked(self):
+
+        def _herons_formula(a, b, c):
+            p = (a + b + c) / 2
+            return sqrt(p * (p-a)*(p-b)*(p-c))
+
+        a = self.ui.lineEdit.text().setValidator("009")
+        b = self.ui.lineEdit_2.text()
+        
+        
+        #self.ui.label_5.setText("4")
+        self.ui.label_5.setText(a)
+        #self.ui.textBrowser.adjustSize()
+
 app = QtWidgets.QApplication([])
 application = mywindow()
 application.show()
