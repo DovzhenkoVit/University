4122fc59c10e4a8e34f644f1e45d7d7fd13c025a
DovzhenkoVit <dovzhenko.vit@gmail.com>
2020-04-24 12:00:32 +0300

    some changes in Lab2 and .ui files

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -3,6 +3,7 @@
 # Розрахувати значення x, визначивши і використавши відповідну функцію
 
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.uic import loadUi
 
 from math import sqrt
@@ -14,19 +15,29 @@ class mywindow(QtWidgets.QMainWindow):
         super(__class__, self).__init__()
         loadUi("Lab2_view.ui", self)
 
-        self.pushButton.clicked.connect(self.btnClicked)
+        self.pushButton.clicked.connect(
+            lambda: self.btnClicked(self.checkBox.isChecked())
+            )
 
     @staticmethod
     def my_formula(a):
         return (sqrt(a) + a) / 2
 
-    def btnClicked(self):
-        a = mywindow.my_formula(7)
-        b = mywindow.my_formula(14)
-        c = mywindow.my_formula(24)
-        ans = a + b + c
-
-        self.ans_label.setText(str(ans))
+    def btnClicked(self, chk):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        if chk:
+            a = mywindow.my_formula(7)
+            b = mywindow.my_formula(14)
+            c = mywindow.my_formula(24)
+            ans = a + b + c
+            msg.setText("Результат = " + str(round(ans, 2)))
+            msg.exec()
+            self.ans_label.setText(str(round(ans, 2)))
+        else:
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Заборонений показ результату")
+            msg.exec()
 
 
 app = QtWidgets.QApplication([])
